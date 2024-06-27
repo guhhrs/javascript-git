ae2d4b61ebbc8532cfdd7b0ace03809fa7a39f0d
guhhrs <gustavooliveirasilvabispo@gmail.com>
2024-06-27 08:18:58 -0300

    adicionando os cáuculos

---
+++ 27-06-2024/calc.js
@@ -0,0 +1,16 @@
+// declaração de variável
+let num1 = 10; 
+let num2 = 5; 
+
+//operção matemáticas
+let soma = num1 + num2; 
+let sub = num1 - num2;
+let mult = num1 * num2; 
+let div = num1 / num2;
+
+//exibindo os resultados no console
+
+console.log("O resultado da soma é: " + soma); 
+console.log("O resultado da subtração é: " + sub); 
+console.log("O resultado da multiplicação é: " + mult); 
+console.log("O resultado da divisão é: " + div); 
